15a4e56e243938faa18f2d22740e049eda824f7b
Doug Sheridan <dsherida@asu.edu>
2020-04-02 21:13:48 -0700

    Day 1 - Variables, simple data types, and string interpolation

---
+++ Day 1.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+// strings and numbers
+var str = "Hello, playground"
+var age = 38
+var population = 8_000_00
+
+// multiline strings
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// double and boolean
+var pi = 3.141
+var awesome = true
+
+// string interpolation
+var score = 85
+var str3 = "Your score was \(score)"
+var results = "The test results are here: \(str)"
+
+// constants
+let taylor = "swift"
+
+// type annotation
+let str4 = "Hello, playground" // <- type inference
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+var burns = """
+The best laid schemes
+o' mice an' men
+gang aft agley
+"""
